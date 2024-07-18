263f911b5149d469abdbf4c212bb8045af201cb2
tumana777 <tumana991@gmail.com>
2024-07-18 16:18:15 +0400

    Add password change and reset logic

--- E-commerce Web Site/technic_shop/settings.py
+++ E-commerce Web Site/technic_shop/settings.py
@@ -129,3 +129,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index'
+
+# Password Reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '95c1645303c0e8'
+EMAIL_HOST_PASSWORD = 'caf9ed50a97248'
--- E-commerce Web Site/users/urls.py
+++ E-commerce Web Site/users/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from .views import UserRegisterView, UserLoginView, UserLogoutView, profile
+from django.contrib.auth.views import (PasswordResetView, PasswordResetDoneView, 
+                                       PasswordResetConfirmView, PasswordResetCompleteView, 
+                                       PasswordChangeView, PasswordChangeDoneView)
 
 urlpatterns = [
     path("user/register/", UserRegisterView.as_view(), name="register"),
     path("user/login/", UserLoginView.as_view(), name="login"),
     path("user/logout/", UserLogoutView.as_view(), name="logout"),
+    path("user/password_reset/", PasswordResetView.as_view(template_name='users/password_reset.html'), name="password_reset"),
+    path("user/password_reset/done/", PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
+    path("user/password_reset_confirm/<uidb64>/<token>/", PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name="password_reset_confirm"),
+    path("user/password_reset_complete/", PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name="password_reset_complete"),
     path("user/profile/", profile, name="profile"),
+    path("user/password_change/", PasswordChangeView.as_view(template_name='users/password_change.html/'), name="password_change"),
+    path("user/password_change/done/", PasswordChangeDoneView.as_view(template_name='users/password_change_done.html/'), name="password_change_done"),
 ]
